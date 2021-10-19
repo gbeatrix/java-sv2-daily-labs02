9421bbe33b7b3300f52fc23fb62e64fb0c684043
gbeatrix <gatibeatrix+github@gmail.com>
2021-10-19 15:39:38 +0200

    napi feladat: téglalap osztály

---
+++ week02-labs-tasks/src/main/java/day02/Geometry.java
@@ -0,0 +1,9 @@
+package day02;
+
+public class Geometry {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(6, 24);
+
+        System.out.println(rectangle.calculateArea());
+    }
+}
